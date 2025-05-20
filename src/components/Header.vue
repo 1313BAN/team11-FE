54fcbbd2cebe3b1030f23878fa8970ec2431b26e
lsn5963 <lsn5963@naver.com>
2025-05-20 15:59:51 +0900

    통신

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header
+    class="w-full h-14 bg-gray-800 text-white flex justify-between items-center px-6 fixed top-0 z-50"
+  >
+    <div class="font-bold text-xl">
+      <router-link to="/">Goldentime</router-link>
+    </div>
+    <nav class="space-x-4 whitespace-nowrap">
+      <router-link to="/">Home</router-link>
+      <router-link to="/map">Map</router-link>
+      <router-link to="/board">Board</router-link>
+      <router-link to="/mypage">My Page</router-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,49 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MapView from '../views/MapView.vue'
+import DetailView from '../views/DetailView.vue'
+import BoardView from '../views/BoardView.vue'
+import MypageView from '../views/MypageView.vue'
+import CalendarView from '../views/CalendarView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+  },
+  {
+    path: '/map',
+    name: 'map',
+    component: MapView,
+  },
+  {
+    path: '/detail/:spotId',
+    name: 'detail',
+    component: DetailView,
+    props: true,
+  },
+  {
+    path: '/calendar/:spotId',
+    name: 'calendar',
+    component: CalendarView,
+    props: true,
+  },
+  {
+    path: '/board',
+    name: 'board',
+    component: BoardView,
+  },
+  {
+    path: '/mypage',
+    name: 'mypage',
+    component: MypageView,
+  },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
